d7805275789978e837f25d6c31e7f697ca2f773a
Adriano Domingues <54463264+programadormovel@users.noreply.github.com>
2023-04-24 14:03:44 -0300

    Aula 3 condicionais e operadores lógicos e relacionais.

--- aula3.py
+++ aula3.py
@@ -1,3 +1,13 @@
+a = float(input('Primeiro bimestre: '))
+b = float(input('Segundo bimestre: '))
+c = float(input('Terceiro bimestre: '))
+d = float(input('Quarto bimestre: '))
+media = (a + b + c + d) / 4
+if a<=10 and b<=10 and c<=10 and d<=10:
+    print('Média: {}'.format(media))
+else:
+    print('foi informada alguma nota errada')
+
 # a = int(input('Primeiro valor: '))
 # b = int(input('Segundo valor: '))
 # c = int(input('Terceiro valor: '))
@@ -11,4 +21,14 @@
 #
 # print('final do programa')
 
-a = int(input('Entre com um valor: '))
+# a = int(input('Entre com um valor: '))
+# b = int(input('Entre com outro valor: '))
+#
+# resto_a = a % 2
+# resto_b = b % 2
+#
+# if resto_a == 0 or not resto_b > 0:
+#     print('foi digitado um número par')
+# else:
+#     print('Nenhum Número par foi digitado')
+
